39b80fc5c547a6f06f0a133ad67b2d649a17e4bb
infiniteblock <subaru_gt@windowslive.com>
2024-02-21 06:30:26 +1300

    Update TileEntityRendererReplicator.java
    Remove Unused imports

--- src/main/java/matteroverdrive/client/render/tileentity/TileEntityRendererReplicator.java
+++ src/main/java/matteroverdrive/client/render/tileentity/TileEntityRendererReplicator.java
@@ -1,16 +1,12 @@
 package matteroverdrive.client.render.tileentity;
 
-import matteroverdrive.Reference;
 import matteroverdrive.tile.TileEntityMachineReplicator;
-import matteroverdrive.util.RenderUtils;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.AdvancedModelLoader;
-import net.minecraftforge.client.model.IModelCustom;
+
 import org.lwjgl.opengl.GL11;
 
 /**
